fbd807efba0ede8c020c8cf1f5223682030893fd
aoyulong <aoyulong@outlook.com>
2026-01-05 02:01:57 +0800

    Initial commit

---
+++ flagscale/train/megatron/training/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2024, NVIDIA CORPORATION. All rights reserved.
+
+import torch
+
+from .global_vars import (
+    get_adlr_autoresume,
+    get_args,
+    get_one_logger,
+    get_signal_handler,
+    get_tensorboard_writer,
+    get_timers,
+    get_tokenizer,
+    get_wandb_writer,
+)
+from .initialize import initialize_megatron
+from .utils import is_last_rank, print_rank_0, print_rank_last
