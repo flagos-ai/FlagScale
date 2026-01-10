225cf5a3be17b0f856dcbb7aa93d389ae4dcb842
Yulong Ao <aoyulong@outlook.com>
2026-01-10 11:37:04 +0800

    Remove some tests temporarily

--- tests/scripts/functional_tests/test_all.sh
+++ tests/scripts/functional_tests/test_all.sh
@@ -6,27 +6,7 @@ commands=(
     "tests/scripts/functional_tests/test_task.sh --type train --task aquila"
     "tests/scripts/functional_tests/test_task.sh --type train --task deepseek"
     "tests/scripts/functional_tests/test_task.sh --type train --task mixtral"
-    "tests/scripts/functional_tests/test_task.sh --type train --task llava_onevision"
     "tests/scripts/functional_tests/test_task.sh --type hetero_train --task aquila"
-    
-    # For inference
-    "tests/scripts/functional_tests/test_task.sh --type inference --task deepseek_r1_distill_qwen"
-    "tests/scripts/functional_tests/test_task.sh --type inference --task deepseek_r1_distill_qwen-flaggems"
-    "tests/scripts/functional_tests/test_task.sh --type inference --task deepseek_r1_distill_qwen-metax"
-    "tests/scripts/functional_tests/test_task.sh --type inference --task deepseek_r1_distill_qwen-flaggems-metax"
-    "tests/scripts/functional_tests/test_task.sh --type inference --task opi_llama3_1_instruct-metax"
-    "tests/scripts/functional_tests/test_task.sh --type inference --task opi_llama3_1_instruct-flaggems-metax"
-    "tests/scripts/functional_tests/test_task.sh --type inference --task qwen3"
-    "tests/scripts/functional_tests/test_task.sh --type inference --task qwen3-flaggems"
-    "tests/scripts/functional_tests/test_task.sh --type inference --task qwen3-metax"
-    "tests/scripts/functional_tests/test_task.sh --type inference --task qwen3-flaggems-metax"
-    "tests/scripts/functional_tests/test_task.sh --type inference --task robobrain2-metax"
-    "tests/scripts/functional_tests/test_task.sh --type inference --task robobrain2-flaggems-metax"
-    "tests/scripts/functional_tests/test_task.sh --type inference --task robobrain2"
-    "tests/scripts/functional_tests/test_task.sh --type inference --task robobrain2-flaggems"
-    # For serve
-    "tests/scripts/functional_tests/test_task.sh --type serve --task qwen2_5"
-    "tests/scripts/functional_tests/test_task.sh --type serve --task base"
 )
 
 for cmd in "${commands[@]}"; do
