4eb7663a4d1516a8fb6c804a6c6a7610a6a0bc2f
MC952-arch <MC952-arch@qq.com>
2026-03-12 11:15:42 +0800

    fix

--- flagscale/serve/__init__.py
+++ flagscale/serve/__init__.py
@@ -1,4 +1,3 @@
 from .utils import load_args, task_config
-from .websocket_policy_server import ServablePolicy
 
-__all__ = ["task_config", "load_args", "ServablePolicy"]
+__all__ = ["task_config", "load_args"]
